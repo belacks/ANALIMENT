0eac6bbde64f15b7af8d444726805c917d52bac4
belacks <aliashari0304@gmail.com>
2025-03-09 00:03:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def predict_sentiment(text, model, vectorizer):
     probability = model.predict_proba(text_vector)[0]
     
     # Get sentiment label and confidence
-    sentiment = 'Positif' if prediction == 0 else 'Negatif'
+    sentiment = 'Negatif' if prediction == 0 else 'Positif'
     confidence = probability[1] if sentiment == 'Positif' else probability[0]
     
     return sentiment, confidence
